ce109f09503cfa4e4da2951bff238a5517648aa1
Dipeshlks <dipesh.ramteke@lakshmisri.com>
2023-03-06 16:29:13 +0530

    server changes_3

--- test_heroku/settings.py
+++ test_heroku/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 import os
 from pathlib import Path
-import dj_database_url
+#import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-so1j*)okxh5pap*)zq9^5_7z1y*zd8zh9+df15=m^6tuz9lr%p
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '0.0.0.0', '*']
 
 
 # Application definition
